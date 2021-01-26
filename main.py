4651d3468345f7e86fa3d6ea2205bdbd718b4361
traf-and <artem.polikarpov00@gmail.com>
2021-01-26 16:07:10 +0300

    func open_file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import Toplevel, filedialog 
 import numpy as np
+from numpy.lib.npyio import loadtxt
 
 class Window():
     class Entr_win(tk.Toplevel):
@@ -25,7 +26,7 @@ class Window():
             self.mainloop
         def ins(self):
             for i in self.name:
-                self.str_in.append(i.get())
+                self.str_in.append(int(i.get())-1)
             self.destroy()
 
 
@@ -36,14 +37,16 @@ class Window():
 
             self.path = None # file path
             self.nn_in = [] # file IN
-            self.nn_out = None # file OUT
-            self.pred = None # preditcion
+            self.nn_out = [] # file OUT
+            self.pred = []
+            self.pred = [] # preditcion
             self.title("NN")
             button_open=tk.Button(self,text='Open File',width=25,height=3,font='arial 14', command=self.open_file)
             button_lm=tk.Button(self,text='NN LM',width=25,height=3,font='arial 14')
             button_adam=tk.Button(self,text='NN ADAM',width=25,height=3,font='arial 14')
             button_lstm=tk.Button(self,text='NN LSTM',width=25,height=3,font='arial 14')
             button_pred=tk.Button(self,text='Predict',width=25,height=3,font='arial 14')
+            button_test=tk.Button(self,text='Test',width=25,height=3,font='arial 14')
             #button_lin=tk.Button(self,text='Text processing',width=25,height=3,font='arial 14')
             button_script=tk.Button(self,text='Script',width=25,height=3,font='arial 14')
             button_close=tk.Button(self,text='Clsoe app',width=25,height=3,font='arial 14')
@@ -53,24 +56,52 @@ class Window():
             button_adam.pack()
             button_lstm.pack()
             button_pred.pack()
+            button_test.pack()
             #button_lin.pack()
             button_script.pack()
             button_close.pack()
 
         def open_file(self):
-            path_to_file = tk.filedialog.askopenfilename()
-            self.path = path_to_file
+            self.path  = tk.filedialog.askopenfilename()
             f = open(self.path).readline()
             a=len(f.split())
             in_win=Window.Entr_win(num_fld=2,lab_txt=["IN start", "IN end"], txt_fld=["1", a-1], title_txt="IN")
             self.wait_window(in_win)
+            if not in_win.str_in:
+                return
             self.nn_in=in_win.str_in
             out_win=Window.Entr_win(num_fld=2,lab_txt=["OUT start", "OUT end"], txt_fld=[a, a], title_txt="OUT")
             self.wait_window(out_win)
+            if not out_win.str_in:
+                return
             self.nn_out=out_win.str_in
-            
-
-
+            a=np.loadtxt(self.path, unpack=True)
+            #print(a[self.nn_in[0]:self.nn_in[-1], : ])
+            self.nn_in=a[self.nn_in[0] : self.nn_in[-1]+1, :]
+            self.nn_out=a[self.nn_out[0] : self.nn_out[-1]+1, :]
+        
+        def but_lm(self):
+            pass
+        
+        def but_adam(self):
+            pass
+        
+        def but_lstm(self):
+            pass
+        
+        def but_pred(self):
+            pass
+        
+        def but_test(self):
+            pass
+        """
+        def but_lin(self):
+            pass
+        """
+        def but_script(self):
+            pass
+        def but_clsoe(self):
+            pass
 
 
 root = Window.Top()
